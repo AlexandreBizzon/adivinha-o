baadf87bd2d0b349c39e01ddfdad39d106ffcdd4
Alexandre Bizzon <112921735+AlexandreBizzon@users.noreply.github.com>
2022-09-17 01:58:51 -0300

    Update Jogo da adinhação.cpp

--- Jogo da adinhação.cpp
+++ Jogo da adinhação.cpp
@@ -17,7 +17,7 @@ int main()
 	printf("***********************************\n");
 	printf("Escolha a dificuldade do jogo\n(1) Bebe-(20 tentativas)\n(2) Adulto-(15 tentativas)\n(3) Master-(10 tentativas)\n(4) Expert-(5 tentativas)");
 	printf("\n");
-	scanf("%d", &dificuldade);
+		scanf("%d", &dificuldade);
 
 	switch(dificuldade){
 		case 1:
@@ -34,11 +34,11 @@ int main()
 	}
 	double pontos = 1000;
 		srand(time(0));
-		numerosecreto = rand() % 100;
+			numerosecreto = rand() % 100;
 
 	for(int i=1; i<=tentativas; i++) {
 		printf("Qual e o seu chute?\n ");
-		scanf("%d", &chute);
+			scanf("%d", &chute);
 		printf("Seu chute foi %d\n", chute);
 			
 		if (chute == numerosecreto){
@@ -54,4 +54,4 @@ int main()
 			scanf(" %c", &continuarJogando);
 			system("cls");
 	}while (continuarJogando == 's');
-	}
+}
